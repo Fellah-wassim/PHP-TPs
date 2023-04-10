8c398d0dae551848abbbb58981d4856d0df7928f
wassim <wbvb41@gmail.com>
2023-04-10 16:47:45 +0200

    .

---
+++ TP2/exo2/calculator.php
@@ -0,0 +1,83 @@
+<?php
+
+  if (isset($_GET['operand1']) && isset($_GET['operand2']) && isset($_GET['operator']) &&
+      !empty($_GET['operand1']) && !empty($_GET['operand2']) && !empty($_GET['operator'])) {
+    
+    $operand1 = $_GET['operand1'];
+    $operand2 = $_GET['operand2'];
+    $operator = $_GET['operator'];
+
+    switch($operator) {
+      case "+":
+        $result = $operand1 + $operand2;
+        break;
+      case "-":
+        $result = $operand1 - $operand2;
+        break;
+      case "x":
+        $result = $operand1 * $operand2;
+        break;
+      case "/":
+        if ($operand2 == 0) {
+          $error = "Impossible! Division by zero.";
+        } else {
+          $result = $operand1 / $operand2;
+        }
+        break;
+      default:
+        $error = "Invalid operator!";
+    }
+  } else {
+    $error = "The two operands and operator must be entered.";
+  }
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Calculator Result</title>
+    <style>
+      body {
+        font-family: Arial, sans-serif;
+        background-color: #f2f2f2;
+        color: #333;
+        margin: 0;
+        padding: 0;
+      }
+      
+      h1 {
+        text-align: center;
+        font-size: 36px;
+        margin-top: 50px;
+      }
+      
+      p {
+        text-align: center;
+        font-size: 24px;
+        margin-top: 30px;
+      }
+      
+      a {
+        display: block;
+        text-align: center;
+        margin-top: 50px;
+      }
+
+      .error {
+        color: red;
+        margin: 20px;
+      }
+      </style>
+  </head>
+  <body>
+    <?php if (isset($error)) { ?>
+      <h1>Error</h1>
+      <p class="error"><?php echo $error; ?></p>
+    <?php } else { ?>
+      <h1>Calculation Result</h1>
+      <p>The result of <?php echo $operand1 . " " . $operator . " " . $operand2 . " is  <span style='font-weight:bold; font-size:24px'>" . $result . "</span>" ?></p>
+    <?php } 
+    echo "<a href='index.html'>&#8592; Back to Form</a>";
+    ?>
+  </body>
+</html>
---
+++ TP2/exo3/calcul.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Table</title>
+    <style>
+      body {
+        font-family: Arial, sans-serif;
+        background-color: #f2f2f2;
+        color: #333;
+        margin: 0;
+        padding: 0;
+      }
+
+      h1 {
+        text-align: center;
+        font-size: 36px;
+        margin-top: 50px;
+      }
+
+      table {
+        border-collapse: collapse;
+        margin: 50px auto;
+        font-size: 16px;
+      }
+      
+      th, td {
+        border: 1px solid black;
+        padding: 10px;
+        text-align: center;
+      }
+      
+      th {
+        background-color: #4CAF50;
+        color: white;
+      }
+
+      p {
+        text-align: center;
+        font-size: 24px;
+        margin-top: 30px;
+      }
+
+      a {
+        display: block;
+        text-align: center;
+        margin-top: 50px;
+      }
+      
+      .error {
+        color: red;
+        margin: 20px;
+      }
+    </style>
+  </head>
+  <body>
+    <?php
+      $error = "";
+      
+      if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+        $n = $_POST["n"];
+        $p = $_POST["p"];
+        
+        if (empty($_POST['n']) || empty($_POST['p'])){
+          $error = "You must enter values for both n and p.";
+        }
+        elseif (!ctype_digit($n) || !ctype_digit($p) || $n <= 0 || $p <= 0) {
+          $error = "You must enter positive integer values.";
+        }
+        elseif ($n >= $p) {
+          $error = "The value of n must be less than that of p.";
+        }
+        else {
+          echo "<table>";
+          echo "<tr><th></th>";
+          for ($col = 1; $col <= $p; $col++) {
+            echo "<th>$col</th>";
+          }
+          echo "</tr>";
+          for ($row = 1; $row <= $n; $row++) {
+            echo "<tr><th>$row</th>";
+            for ($col = 1; $col <= $p; $col++) {
+              echo "<td>" . ($row * $col) . "</td>";
+            }
+            echo "</tr>";
+          }
+          echo "</table>";
+        }
+      }
+    ?>
+    <?php if (!empty($error)): ?>
+      <h1>Error</h1>
+      <p class="error"><?php echo $error; ?></p>
+    <?php endif; ?>
+    <a href='index.html'>&#8592; Back to Form</a>;
+  </body>
+</html>
