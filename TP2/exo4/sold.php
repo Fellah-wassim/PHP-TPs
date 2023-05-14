527222fea1059c9fed0596ed4c513e4c6bbe6ec2
wassim <wbvb41@gmail.com>
2023-05-14 22:49:24 +0200

    corrected the exo 4 formula to calculate the balance

--- TP2/exo4/sold.php
+++ TP2/exo4/sold.php
@@ -13,45 +13,45 @@
         padding: 0;
       }
       h1 {
-  text-align: center;
-  font-size: 36px;
-  margin-top: 50px;
-}
+        text-align: center;
+        font-size: 36px;
+        margin-top: 50px;
+      }
 
-table {
-  border-collapse: collapse;
-  margin: 50px auto;
-  font-size: 16px;
-}
+      table {
+        border-collapse: collapse;
+        margin: 50px auto;
+        font-size: 16px;
+      }
 
-th,
-td {
-  border: 1px solid black;
-  padding: 10px;
-  text-align: center;
-}
+      th,
+      td {
+        border: 1px solid black;
+        padding: 10px;
+        text-align: center;
+      }
 
-th {
-  background-color: #4caf50;
-  color: white;
-}
+      th {
+        background-color: #4caf50;
+        color: white;
+      }
 
-p {
-  text-align: center;
-  font-size: 24px;
-  margin-top: 30px;
-}
+      p {
+        text-align: center;
+        font-size: 24px;
+        margin-top: 30px;
+      }
 
-a {
-  display: block;
-  text-align: center;
-  margin-top: 50px;
-}
+      a {
+        display: block;
+        text-align: center;
+        margin-top: 50px;
+      }
 
-.error {
-  color: red;
-  margin: 20px;
-}
+      .error {
+        color: red;
+        margin: 20px;
+      }
 
     </style>
     <title>Exo 4 - Results</title>
@@ -74,9 +74,13 @@ a {
     } elseif (empty($years)) {
       $error = "The number of years must be selected.";
     } else {
+      $final_balance = $balance;
       $interest_decimal = $interest / 100;
-      $interest_earned = $balance * $interest_decimal * $years;
-      echo "<p>You will earn $interest_earned in interest over $years years with a starting balance of $balance and an interest rate of $interest%.</p>";
+      for($index=1; $index<=$years; $index++){
+        $final_balance = $final_balance+($final_balance * $interest_decimal);
+      }
+      echo "<p>You final sold is $final_balance </p>";
+
     }
     ?>
     <?php if (!empty($error)): ?>
